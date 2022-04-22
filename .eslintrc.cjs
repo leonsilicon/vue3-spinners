56b00ac2a1130046e210557561714d85663ea419
Leon Si <contact@leonzalion.com>
2022-04-22 13:56:56 -0400

    fix: lint errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,4 +4,5 @@ const { defineConfig } = require('eslint-define-config');
 module.exports = defineConfig({
 	extends: require.resolve('@leonzalion/configs/eslint.cjs'),
 	parserOptions: { project: path.resolve(__dirname, 'tsconfig.eslint.json') },
+
 });
--- demo/.eslintrc.cjs
+++ demo/.eslintrc.cjs
@@ -1,14 +1,17 @@
-const path = require('path');
 const { defineConfig } = require('eslint-define-config');
+const path = require('path');
 
 module.exports = defineConfig({
 	extends: '../.eslintrc.cjs',
-	parserOptions: { project: path.resolve(__dirname, './tsconfig.eslint.json') },
+	parserOptions: {
+		project: path.resolve(__dirname, './tsconfig.eslint.json'),
+		extraFileExtensions: ['vue'],
+	},
 	rules: {
 		'import/no-extraneous-dependencies': [
 			'error',
 			{
-				packageDir: [__dirname, path.resolve(__dirname, '..')],
+				packageDir: [path.resolve(__dirname, '..')],
 			},
 		],
 	},
