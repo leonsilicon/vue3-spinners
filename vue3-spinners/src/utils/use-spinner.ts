dcf566936f317deb837e5b2e19ebe78b4ec752e3
Leon Si <contact@leonzalion.com>
2022-04-22 13:47:46 -0400

    chore: update deps

---
+++ vue3-spinners/src/utils/use-spinner.ts
@@ -0,0 +1,20 @@
+import type { ExtractPropTypes } from 'vue';
+import { computed } from 'vue';
+
+export const spinnerProps = {
+	size: {
+		type: [Number, String],
+		default: '1em',
+	},
+	color: String,
+};
+
+export default function useSpinner(
+	props: ExtractPropTypes<typeof spinnerProps>
+) {
+	return {
+		cSize: computed(() => props.size),
+
+		classes: computed(() => 'q-spinner'),
+	};
+}
--- vue3-spinners/vitest.config.ts
+++ vue3-spinners/vitest.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "vitest/config";
 import { join } from 'desm';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	resolve: {
 		alias: {
 			'~': join(import.meta.url, 'src'),
 			'~test': join(import.meta.url, 'test'),
-		}
+		},
 	},
-})
+});
