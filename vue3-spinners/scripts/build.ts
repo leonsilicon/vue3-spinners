0f43a5a2af4715f9ab85583b8c33d277c7ef793a
Leon Si <contact@leonzalion.com>
2022-04-23 11:20:03 -0400

    chore: update readme

--- vue3-spinners/scripts/build.ts
+++ vue3-spinners/scripts/build.ts
@@ -1,12 +1,5 @@
 import { execaCommandSync as exec } from 'execa';
-import {
-	chProjectDir,
-	copyPackageFiles,
-	getProjectDir,
-	rmDist,
-} from 'lion-system';
-import * as fs from 'node:fs';
-import * as path from 'node:path';
+import { chProjectDir, copyPackageFiles, rmDist } from 'lion-system';
 
 chProjectDir(import.meta.url);
 rmDist();
@@ -16,5 +9,3 @@ await copyPackageFiles({
 	additionalFiles: ['src/spinners.css'],
 	commonjs: { external: /\/vue\// },
 });
-const projectDir = getProjectDir(import.meta.url, { monorepoRoot: true });
-fs.cpSync(path.join(projectDir, 'assets'), 'dist/assets', { recursive: true });
