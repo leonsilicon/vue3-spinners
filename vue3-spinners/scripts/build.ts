fa6b5b362ad7bbad87a9cacc296755b92d746172
Leon Si <contact@leonzalion.com>
2022-04-23 10:37:36 -0400

    chore: tweak build script

--- vue3-spinners/scripts/build.ts
+++ vue3-spinners/scripts/build.ts
@@ -1,5 +1,12 @@
 import { execaCommandSync as exec } from 'execa';
-import { chProjectDir, copyPackageFiles, rmDist } from 'lion-system';
+import {
+	chProjectDir,
+	copyPackageFiles,
+	getProjectDir,
+	rmDist,
+} from 'lion-system';
+import * as fs from 'node:fs';
+import * as path from 'node:path';
 
 chProjectDir(import.meta.url);
 rmDist();
@@ -9,3 +16,5 @@ await copyPackageFiles({
 	additionalFiles: ['src/spinners.css'],
 	commonjs: { external: /\/vue\// },
 });
+const projectDir = getProjectDir(import.meta.url, { monorepoRoot: true });
+fs.cpSync(path.join(projectDir, 'assets'), 'dist/assets', { recursive: true });
