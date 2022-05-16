f62386ca4d20f9ab7ab1c31a51f0fff54e816207
Leon Si <contact@leonzalion.com>
2022-05-16 12:07:15 -0400

    chore: update configs

--- packages/vue3-spinners/typings/style-inject.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'style-inject' {
-	interface StyleInjectOptions {
-		insertAt?: 'top';
-	}
-
-	declare function styleInject(css: string, options?: StyleInjectOptions): void;
-
-	export default styleInject;
-}
