054061e93c597aa2ea1dec24568bb184b3eadfea
Leon Si <leon@leonsilicon.com>
2025-04-08 13:24:11 -0400

    fix

--- packages/vue3-spinners/build.config.ts
+++ packages/vue3-spinners/build.config.ts
@@ -1,12 +1,11 @@
+import fs from "node:fs";
 import { defineBuildConfig } from "unbuild";
 import Vue from "unplugin-vue/rollup";
 import VueMacros from "vue-macros/rollup";
 import { join } from "desm";
 
 export default defineBuildConfig({
-  entries: [
-    "src/index.ts"
-  ],
+  entries: ["src/index.ts"],
   rollup: {
     inlineDependencies: true,
   },
@@ -21,6 +20,12 @@ export default defineBuildConfig({
         }),
         ...options.plugins,
       ];
+    },
+    async "build:done"(ctx) {
+      await fs.promises.cp(
+        join(import.meta.url, "../../readme.md"),
+        join(import.meta.url, "readme.md")
+      );
     },
   },
 });
